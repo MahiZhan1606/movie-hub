019e18a9e8733c455e6f41a19b0b30cc62e48994
MahiZhan1606 <t.mahes161197@gmail.com>
2023-03-21 14:16:26 +0530

    New Update

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,29 @@
 import { useState } from 'react';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Badge from '@mui/material/Badge';
+import MailIcon from '@mui/icons-material/Mail';
+
 
 export function Counter() {
     let [like, setLike] = useState(0);
     let [disLike, setDisLike] = useState(0);
 
+    const incrementLike = () => setLike(like + 1);
+    const incrementDisLike = () => setDisLike(disLike + 1);
     return (
         <div>
             {like - disLike >= 10 ? <h3>You are Awesome😍😎</h3> : null}
-            <button onClick={() => setLike(like + 1)}>👍{like}</button>
-            <button onClick={() => setDisLike(like + 1)}>👎{disLike}</button>
+            <IconButton onClick={incrementLike} color="primary" aria-label="like">
+                <Badge badgeContent={like} color="primary">
+                    👍
+                </Badge>
+            </IconButton>
+            <IconButton onClick={incrementDisLike} color="error" aria-label="dislike">
+                <Badge badgeContent={disLike} color="error">
+                    👎
+                </Badge>
+            </IconButton>
         </div>
     );
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,5 +1,9 @@
 import { useState } from 'react';
 import { Counter } from './Counter';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export function Movie({ movie }) {
 
@@ -11,10 +15,16 @@ export function Movie({ movie }) {
         <div className="movie-container">
             <img src={movie.poster} alt={movie.name} className="movie-poster" />
             <div className="movie-specs">
-                <h2 className="movie-name">{movie.name}</h2>
+                <h2 className="movie-name">{movie.name}
+                    <IconButton
+                        color="primary"
+                        onClick={() => setShow(!show)}
+                        aria-label="Toggle Summary">
+                        {show ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+                    </IconButton>
+                </h2>
                 <p style={styles} className="movie-rating">⭐{movie.rating}</p>
             </div>
-            <button onClick={() => setShow(!show)}>Toggle description</button>
             {show ? <p className="movie-summary">{movie.summary}</p> : null}
             <Counter />
         </div>
