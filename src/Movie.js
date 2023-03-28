363bb2adbdf766a6b56a8197ac25edd692c5a4d4
MahiZhan1606 <t.mahes161197@gmail.com>
2023-03-29 02:03:46 +0530

    new Update

--- src/Movie.js
+++ src/Movie.js
@@ -4,13 +4,16 @@ import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import InfoIcon from '@mui/icons-material/Info';
+import { useNavigate } from 'react-router-dom';
 
-export function Movie({ movie }) {
+export function Movie({ movie, id }) {
 
     const styles = {
         color: movie.rating >= 8.5 ? "green" : "red",
     };
     const [show, setShow] = useState(true);
+    const navigate = useNavigate();
     return (
         <div className="movie-container">
             <img src={movie.poster} alt={movie.name} className="movie-poster" />
@@ -21,6 +24,12 @@ export function Movie({ movie }) {
                         onClick={() => setShow(!show)}
                         aria-label="Toggle Summary">
                         {show ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+                    </IconButton>
+                    <IconButton
+                        color="primary"
+                        onClick={() => navigate(`/movies/${id}`)}
+                        aria-label="Toggle Summary">
+                        <InfoIcon />
                     </IconButton>
                 </h2>
                 <p style={styles} className="movie-rating">⭐{movie.rating}</p>
